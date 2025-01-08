a87cc6f82407d0bcc1fc4dca5f4ca5e5fc38b72d
johaco <johacosanchez@gmail.com>
2025-01-08 17:19:36 -0300

    section dieta-balanceada modificada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,25 +44,46 @@ const Home = () => {
         </section>
 
         {/* Dieta Balanceada Section */}
-        <section className="home-section home-dieta section-standard">
-          <div className="dieta-container  section-content">
+        <section className="home-section section-dieta section-standard bg-dark text-white">
+          <div className="dieta-container section-content">
             <div className="dieta-image">
               <img src="/src/assets/dieta-balanceada.png" alt="Dieta Balanceada" />
             </div>
             <div className="dieta-content">
-              <h2 className='text- dark general-title'>Dieta Balanceada</h2>
-              <p className="text-dark general-paragraph">
+              <h2 className="text-start text-white general-title">Dieta Balanceada</h2>
+              <p className="text-start text-white general-paragraph">
                 Una dieta saludable es clave para mantener tu cuerpo en óptimas condiciones. Aprende a equilibrar tus alimentos y conoce cuáles son los nutrientes esenciales para tu bienestar.
               </p>
               <ul className="dieta-tips">
-                <li>Incluye una variedad de frutas y verduras en cada comida.</li>
-                <li>Opta por carbohidratos integrales en lugar de refinados.</li>
-                <li>Consume proteínas magras como pollo, pescado o legumbres.</li>
-                <li>Evita el consumo excesivo de azúcares y grasas saturadas.</li>
+                <li>
+                  <span className="custom-icon">
+                    <i className="fas fa-apple-alt"></i>
+                  </span>
+                  Incluye una variedad de frutas y verduras en cada comida.
+                </li>
+                <li>
+                  <span className="custom-icon">
+                    <i className="fas fa-bread-slice"></i>
+                  </span>
+                  Opta por carbohidratos integrales en lugar de refinados.
+                </li>
+                <li>
+                  <span className="custom-icon">
+                    <i className="fas fa-drumstick-bite"></i>
+                  </span>
+                  Consume proteínas magras como pollo, pescado o legumbres.
+                </li>
+                <li>
+                  <span className="custom-icon">
+                    <i className="fas fa-candy-cane"></i>
+                  </span>
+                  Evita el consumo excesivo de azúcares y grasas saturadas.
+                </li>
               </ul>
             </div>
           </div>
         </section>
+
 
         {/* Ejercicio y Actividad Física Section */}
         <section className="home-section section-standard">
@@ -97,57 +118,57 @@ const Home = () => {
         {/* Control del Estrés Section */}
         <section className="control-estres section-standard">
           <div className='section-content'>
-          <h2 className='text-dark general-title'>Control del Estrés</h2>
-          <div className="video-container">
-            <iframe src="https://youtu.be/iWU1KUCsYOI" title="Control del Estrés Video" frameBorder="0" allowFullScreen></iframe>
-          </div>
-          <div className="consejos">
-            <div className="consejo">
-              <img src="icon-relax.svg" alt="Relájate" />
-              <h4>Técnicas de relajación</h4>
-              <p>Practica la meditación y la respiración profunda para reducir el estrés.</p>
+            <h2 className='text-dark general-title'>Control del Estrés</h2>
+            <div className="video-container">
+              <iframe src="https://youtu.be/iWU1KUCsYOI" title="Control del Estrés Video" frameBorder="0" allowFullScreen></iframe>
             </div>
-            <div className="consejo">
-              <img src="icon-exercise.svg" alt="Ejercicio" />
-              <h4>Ejercicio Regular</h4>
-              <p>Haz ejercicio de manera regular para liberar tensiones y mejorar tu bienestar.</p>
+            <div className="consejos">
+              <div className="consejo">
+                <img src="icon-relax.svg" alt="Relájate" />
+                <h4>Técnicas de relajación</h4>
+                <p>Practica la meditación y la respiración profunda para reducir el estrés.</p>
+              </div>
+              <div className="consejo">
+                <img src="icon-exercise.svg" alt="Ejercicio" />
+                <h4>Ejercicio Regular</h4>
+                <p>Haz ejercicio de manera regular para liberar tensiones y mejorar tu bienestar.</p>
+              </div>
+              <div className="consejo">
+                <img src="icon-walk.svg" alt="Caminar" />
+                <h4>Caminatas al aire libre</h4>
+                <p>Salir a caminar en la naturaleza ayuda a reducir el estrés y mejora tu ánimo.</p>
+              </div>
             </div>
-            <div className="consejo">
-              <img src="icon-walk.svg" alt="Caminar" />
-              <h4>Caminatas al aire libre</h4>
-              <p>Salir a caminar en la naturaleza ayuda a reducir el estrés y mejora tu ánimo.</p>
-            </div>
-          </div>
           </div>
         </section>
 
         {/* Prevención de Enfermedades Section */}
         <section className="prevencion-enfermedades section-standard">
           <div className='section-content'>
-          <h2 className='text-dark general-title'>Prevención de Enfermedades</h2>
-          <div className="checklist">
-            <h3>Chequeos Médicos Importantes</h3>
-            <ul>
-              <li>Chequeo general anual</li>
-              <li>Examen de colesterol y glucosa</li>
-              <li>Vacunación y refuerzos</li>
-              <li>Examen de cáncer según edad</li>
-            </ul>
-          </div>
-          <div className="info-container">
-            <div className="info-item">
-              <h4>Chequeos de Sangre</h4>
-              <p>Realizar exámenes de sangre periódicos para conocer tu estado de salud.</p>
+            <h2 className='text-dark general-title'>Prevención de Enfermedades</h2>
+            <div className="checklist">
+              <h3>Chequeos Médicos Importantes</h3>
+              <ul>
+                <li>Chequeo general anual</li>
+                <li>Examen de colesterol y glucosa</li>
+                <li>Vacunación y refuerzos</li>
+                <li>Examen de cáncer según edad</li>
+              </ul>
             </div>
-            <div className="info-item">
-              <h4>Vacunas y Prevención</h4>
-              <p>Consulta a tu médico sobre las vacunas necesarias y su calendario.</p>
+            <div className="info-container">
+              <div className="info-item">
+                <h4>Chequeos de Sangre</h4>
+                <p>Realizar exámenes de sangre periódicos para conocer tu estado de salud.</p>
+              </div>
+              <div className="info-item">
+                <h4>Vacunas y Prevención</h4>
+                <p>Consulta a tu médico sobre las vacunas necesarias y su calendario.</p>
+              </div>
+              <div className="info-item">
+                <h4>Ejercicio Preventivo</h4>
+                <p>El ejercicio regular no solo mantiene tu cuerpo activo, sino que previene enfermedades.</p>
+              </div>
             </div>
-            <div className="info-item">
-              <h4>Ejercicio Preventivo</h4>
-              <p>El ejercicio regular no solo mantiene tu cuerpo activo, sino que previene enfermedades.</p>
-            </div>
-          </div>
           </div>
         </section>
       </main>
