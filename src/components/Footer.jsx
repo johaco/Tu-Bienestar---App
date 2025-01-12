70bb2857b9ff51ac855c8277eca1aacaffd52e2e
johaco <johacosanchez@gmail.com>
2025-01-12 03:28:00 -0300

    footer terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ import "../styles/Footer.css"; // Si tienes estilos específicos para el footer
 const Footer = () => {
   return (
     <footer className="footer bg-dark text-white text-center py-3">
-      <p>&copy; 2025 IMC Portal. Todos los derechos reservados.</p>
+      <img
+        src="/src/assets/logo2.png"
+        className="img-fluid rounded-top mb-3"
+        alt="logo tu bienestar"
+      />
+      <p>&copy; 2025 Tu Bienestar. Todos los derechos reservados.</p>
     </footer>
   );
 };
